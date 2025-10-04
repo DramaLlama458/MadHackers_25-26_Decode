d0803d62961a1ee3622d23377ae0c403a17c1708
Oliver Casas Dorn-Havlik <olivercasasdorn@gmail.com>
2025-10-04 10:44:59 -0500

    Changed some small stuff for roadrunner setup

--- TeamCode/src/main/java/RobotConstants.java
+++ TeamCode/src/main/java/RobotConstants.java
@@ -1,11 +1,15 @@
 import com.acmerobotics.dashboard.config.Config;
-
+import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 @Config
 public class RobotConstants {
+
     /*
     Examples of how to code constants that FTC Dashboard can change:
     public static int MAGIC_NUMBER = 32;
     public static PIDCoefficients TURNING_PID = new PIDCoefficients();
     */
+
+
 
 }
