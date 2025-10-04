a55877f2886125a13001ade8a4b60ca6e83ef262
Winston's Macbook <119084558+DerjenigeUberMensch@users.noreply.github.com>
2025-10-04 11:17:36 -0500

    Implemented Gamepad.java

--- TeamCode/src/main/java/Gamepad.java
+++ TeamCode/src/main/java/Gamepad.java
@@ -5,96 +5,177 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-
+import java.lang.reflect.Field;
+import java.util.function.Consumer;
+import java.util.*;
 
 
 public class Gamepad {
+    public static Map<String, Consumer<Object>> input = new HashMap<>();
+
+    // Init code stuff
+    static 
+    {
+        // Axis (Float)
+        input.put("left_stick_x", o -> Axis0((Float) o));
+        input.put("left_stick_y", o -> Axis1((Float) o));
+        input.put("right_stick_x", o -> Axis2((Float) o));
+        input.put("right_stick_y", o -> Axis3((Float) o));
+
+        // Buttons (Boolean)
+        input.put("x", o -> Button0((Boolean) o));
+        input.put("a", o -> Button1((Boolean) o));
+        input.put("b", o -> Button2((Boolean) o));
+        input.put("y", o -> Button3((Boolean) o));
+        input.put("left_bumper", o -> Button4((Boolean) o));
+        input.put("right_bumper", o -> Button5((Boolean) o));
+        input.put("left_trigger", o -> Button6((Boolean) o));
+        input.put("right_trigger", o -> Button7((Boolean) o));
+        input.put("back", o -> Button8((Boolean) o));
+        input.put("start", o -> Button9((Boolean) o));
+        input.put("left_stick_button", o -> Button10((Boolean) o));
+        input.put("right_stick_button", o -> Button11((Boolean) o));
+        input.put("dpad_up", o -> DpadUp((Boolean) o));
+        input.put("dpad_down", o -> DpadDown((Boolean) o));
+        input.put("dpad_left", o -> DpadLeft((Boolean) o));
+        input.put("dpad_right", o -> DpadRight((Boolean) o));
+    }
+
+    
+    public static Gamepad GetGamePad1()
+    {   return gamepad1;
+    }
+
+    public static Gamepad GetGamePad2()
+    {   return gamepad2;
+    }
+
     // Left Joystick X
-    public void Axis0()
+    public static void Axis0(float xAxis)
     {
     }
     // Left Joystick Y (inv)
-    public void Axis1()
+    public static void Axis1(float yAxis)
     {
     }
     // Right Joystick X
-    public void Axis2()
+    public static void Axis2(float xAxis)
     {
     }
     // Right Joystick Y (inv)
-    public void Axis3()
+    public static void Axis3(float yAxis)
     {
     }
 
-    //  Y
-    public void Button0()
+    //  X
+    public static void Button0(boolean pressed)
     {
     }
 
     // A
-    public void Button1()
+    public static void Button1(boolean pressed)
     {
     }
 
-    // []
-    public void Button2()
+    // []/B (Square)
+    public static void Button2(boolean pressed)
     {
     }
 
-    // X
-    public void Button3()
+    // Y
+    public static void Button3(boolean pressed)
     {
     }
 
     // Left Bumper
-    public void Button4()
+    public static void Button4(boolean pressed)
     {
     }
 
     // Right Bumper
-    public void Button5()
+    public static void Button5(boolean pressed)
     {
     }
 
     // Left Trigger
-    public void Button6()
+    public static void Button6(boolean pressed)
     {
     }
 
     // Right Trigger
-    public void Button7()
+    public static void Button7(boolean pressed)
     {
     }
 
     // Back
-    public void Button8()
+    public static void Button8(boolean pressed)
     {
     }
 
     // Start
-    public void Button9()
+    public static void Button9(boolean pressed)
     {
     }
 
     // Left Joystick
-    public void Button10()
+    public static void Button10(boolean pressed)
     {
     }
 
     // Right Joystick
-    public void Button11()
+    public static void Button11(boolean pressed)
     {
     }
 
-    // Cross
-    public void Hat()
+    public static void DpadUp(boolean pressed)
     {
     }
 
-    public void Loop()
+    public static void DpadDown(boolean pressed)
     {
-        // if (gamepad1.a) Button1();
-        // ...
     }
 
+    public static void DpadLeft(boolean pressed)
+    {
+    }
+
+    public static void DpadRight(boolean pressed)
+    {
+    }
+
+    public static void HandleInput()
+    {
+        Gamepad gamepad = GetGamePad1();
+        Class<?> _class = gamepad.getClass();
+        Consumer<Object> func;
+        String key;
+        Field field;
+        Object value;
+
+        for (Map.Entry<String, Consumer<Object>> i : input.entrySet())
+        {
+            try
+            {
+                key = i.getKey();
+                func = i.getValue();
+
+                field = _class.getDeclaredField(key);
+                value = field.get(gamepad);
+
+                func.accept(value);
+            }
+            catch (Exception e)
+            {   
+                // If its not there??? Then we dont need to do anything.
+            }
+        }
+    }
 }
+
+
+
+
+
+
+
+
