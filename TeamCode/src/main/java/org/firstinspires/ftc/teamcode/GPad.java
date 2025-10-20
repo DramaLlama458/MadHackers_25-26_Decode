06e2698290de2e9f39d333a5c9d230c0e0fb6119
DerjenigeUberMensch <119084558+DerjenigeUberMensch@users.noreply.github.com>
2025-10-20 17:23:48 -0500

    Reverted GPad Changes + Kept new cubic scaling formula.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GPad.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GPad.java
@@ -37,8 +37,10 @@ public class GPad
 
     public double scaleInput(double input) 
     {
-        return input * input * input * 0.8 + input * 0.2;
+        /* old code */
         //return input * input * input;
+
+        return input * input * input * 0.8 + input * 0.2;
     }
     
     public void Joystick(float l_xAxis, float l_yAxis, float r_xAxis, float y_xAxis)
@@ -47,7 +49,6 @@ public class GPad
         double x = scaleInput(l_xAxis) * 1.1; // Counteract imperfect strafing
         double rx = scaleInput(r_xAxis);
 
-        /*
         // Denominator is the largest motor power (absolute value) or 1
         // This ensures all the powers maintain the same ratio,
         // but only if at least one is out of the range [-1, 1]
@@ -56,22 +57,6 @@ public class GPad
         double leftBackPower = (y - x + rx) / denominator;
         double rightFrontPower = (y - x - rx) / denominator;
         double rightBackPower = (y + x - rx) / denominator;
-        */
-        double leftFrontPower = y + x + rx;
-        double leftBackPower = y - x + rx;
-        double rightFrontPower = y - x - rx;
-        double rightBackPower = y + x - rx;
-
-        double maxPower = Math.max(Math.max(Math.abs(leftFrontPower), Math.abs(leftBackPower)), Math.max(Math.abs(rightFrontPower), Math.abs(rightBackPower)));
-
-        if (maxPower < 1.0)
-        {   maxPower = 1.0;
-        }
-
-        leftFrontPower /= maxPower;
-        leftBackPower /= maxPower;
-        rightFrontPower /= maxPower;
-        rightBackPower /= maxPower;
 
         double wheelPowerMultiplier = 0.75;
 
