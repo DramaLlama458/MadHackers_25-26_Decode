fb4b6c876d0b3dce320704890ac52862d9244954
DerjenigeUberMensch <119084558+DerjenigeUberMensch@users.noreply.github.com>
2025-10-20 19:53:16 -0500

    Added test joystick 'Joystick2'

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GPad.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GPad.java
@@ -42,8 +42,8 @@ public class GPad
 
         return input * input * input * 0.8 + input * 0.2;
     }
-    
-    public void Joystick(float l_xAxis, float l_yAxis, float r_xAxis, float r_yAxis)
+
+    public void Joystick(float l_xAxis, float l_yAxis, float r_xAxis, float y_xAxis)
     {
         double y = scaleInput(-l_yAxis); // Remember, Y stick value is reversed
         double x = scaleInput(l_xAxis) * 1.1; // Counteract imperfect strafing
