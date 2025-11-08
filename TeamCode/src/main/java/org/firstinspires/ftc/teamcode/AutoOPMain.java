6e85c55f2e9c83fc77a09e11d96b0b6d61d20897
DerjenigeUberMensch <119084558+DerjenigeUberMensch@users.noreply.github.com>
2025-11-08 09:19:46 -0600

    Autonomous setup

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoOPMain.java
@@ -0,0 +1,46 @@
+package org.firstinspires.ftc.teamcode;
+
+import android.service.controls.Control;
+import com.acmerobotics.roadrunner.*;
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Gamepad;
+
+import java.lang.Math;
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Consumer;
+
+
+@Autonomous
+public class AutoOPMain extends LinearOpMode  {
+    ControlHub hub;
+    FtcDashboard dash = FtcDashboard.getInstance();
+
+    Pose2d START_POSE = new Pose2d(0, 0, Math.toRadians(0));
+    Pose2d TARGET_POSE = new Pose2d(0, 0, Math.toRadians(90));
+    Action path;
+
+
+    @Override
+    public void runOpMode() throws InterruptedException
+    {
+        hub = new ControlHub(hardwareMap, START_POSE);
+        path = hub.drive.actionBuilder(START_POSE)
+                .waitSeconds(1)
+                .strafeToLinearHeading(TARGET_POSE.position, TARGET_POSE.heading)
+                .build();
+        waitForStart();
+
+        while (opModeIsActive() && !isStopRequested())
+        {
+            path.run(new TelemetryPacket());
+            //dash.sendTelemetryPacket(new TelemetryPacket());
+        }
+    }
+}
